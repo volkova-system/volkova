ed040ed1c4d82110c2a56a788fea1d751a991ddb
Richeve Bebedor <richeve.bebedor963+vs@proton.me>
2026-03-31 03:07:43 +0800

    issue(setting): add tcl build tool configuration settings
    1. file: tools/prototype-system/build-tcl-tool/tcl/src/setting.tcl
    2. change: created configuration namespace with platform urls and settings
    3. reason: provide centralized configuration for tcl build tool functionality
    4. impact: no impact, new file with configuration constants only
    5. verify: reviewed file content and tcl namespace structure

---
+++ tools/prototype-system/build-tcl-tool/tcl/src/setting.tcl
@@ -0,0 +1,46 @@
+# Configuration settings for build-tcl tool
+
+namespace eval build_tcl {
+    namespace eval setting {
+        # Command name for CLI interface
+        variable commandName "build-tcl"
+
+        # Directory marker to identify project root (tools directory)
+        variable projectMarker "tools"
+
+        # Maximum directory levels to ascend when searching for root
+        variable maxAscend 12
+
+        # TCL Kit download URLs for different platforms
+        variable tclkitUrls
+        array set tclkitUrls {
+            windows "https://sourceforge.net/projects/twapi/files/Tcl%20binaries/Tclkits/tclkit-gui-8_6_7-x64.exe/download"
+            linux   "https://gorilla.dp100.com/downloads/tclkit/tclkit-8.6.9-linux-x86_64"
+            darwin  "https://gorilla.dp100.com/downloads/tclkit/tclkit-8.6.6-MacOSX-amd64-mk-tk"
+        }
+
+        # SDX (Starkit Developer eXtension) download URLs - needed for building starkits
+        variable sdxUrls
+        array set sdxUrls {
+            windows "https://www.tcl-lang.org/starkits/sdx.kit"
+            linux   "https://www.tcl-lang.org/starkits/sdx.kit"
+            darwin  "https://www.tcl-lang.org/starkits/sdx.kit"
+        }
+
+        # Get current platform
+        proc getCurrentPlatform {} {
+            global tcl_platform
+            switch $tcl_platform(platform) {
+                "windows" { return "windows" }
+                "unix" {
+                    if {$tcl_platform(os) eq "Darwin"} {
+                        return "darwin"
+                    } else {
+                        return "linux"
+                    }
+                }
+                default { return "linux" }
+            }
+        }
+    }
+}
